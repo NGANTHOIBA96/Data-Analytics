5c78c2a88747f4601c9b208f3c778fc59ba005e1
NGANTHOIBA96 <71542444+NGANTHOIBA96@users.noreply.github.com>
2020-09-19 12:01:49 +0530

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -26,15 +26,15 @@ y_train = sc_y.fit_transform(y_train)"""
 
 # Using the elbow method to find the optimal number of clusters
 from sklearn.cluster import KMeans
-wcss = []
+who = []
 for i in range(1, 11):
     kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
     kmeans.fit(X)
-    wcss.append(kmeans.inertia_)
+    who.append(kmeans.inertia_)
 plt.plot(range(1, 11), wcss)
 plt.title('The Elbow Method')
 plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
+plt.ylabel('WHO')
 plt.show()
 
 # Fitting K-Means to the dataset
@@ -52,7 +52,7 @@ plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label
 plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
 #plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
 plt.title('Clusters of customers')
-plt.xlabel('Annual Income (k$)')
+plt.xlabel('Annual Income (Rs )')
 plt.ylabel('Spending Score (1-100)')
 plt.legend()
 plt.show()
